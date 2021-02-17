cb535b32b3148bdceefdd464e81c859f19ecd35b
Natalia Potocekova <natalia.potocekova@yahoo.com>
2021-02-17 17:45:22 +0100

    added classes representing json response for resources request and resource languages request

---
+++ AvailableResourcesCheck/ResourceLanguagesResponse.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AvailableResourcesCheck
+{
+    public class ResourceLanguagesResponse
+    {
+        public string Batchcomplete { get; set; }
+        public Warnings Warnings { get; set; }
+        public Query Query { get; set; }
+    }
+
+    public class MessageGroupStats
+    {        
+        public int Total { get; set; }
+        public int Translated { get; set; }
+        public int Fuzzy { get; set; }
+        public int Proofread { get; set; }
+        public string Code { get; set; }
+        public string Language { get; set; }
+    }
+
+    public class Warnings
+    {
+        public MessageGroupStats Messagegroupstats { get; set; }
+    }
+
+    public class Query
+    {
+        public List<MessageGroupStats> Messagegroupstats { get; set; }
+    }
+}
---
+++ AvailableResourcesCheck/ResourcesResponse.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AvailableResourcesCheck
+{
+    public class ResourcesResponse
+    {
+        public string Batchcomplete { get; set; }
+        public QueryResources Query { get; set; }
+    }
+
+    public class Metadata
+    {
+        public int Resultsize { get; set; }
+        public int Remaining { get; set; }
+    }
+
+    public class Messagecollection
+    {
+        public string Key { get; set; }
+        public string Definition { get; set; }
+        public object Translation { get; set; }
+        public string Title { get; set; }
+        public string PrimaryGroup { get; set; }
+    }
+
+    public class QueryResources
+    {
+        public Metadata Metadata { get; set; }
+        public List<Messagecollection> Messagecollection { get; set; }
+    }
+   
+}
